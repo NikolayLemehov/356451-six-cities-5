cbdfccdf9dba311af3bc4a87f843e00f0b9fb0bd
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-10-09 20:50:09 +0300

    Добавляет Link для лого подвала страницы favorites.jsx

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -71,9 +71,9 @@ const Favorites = (props) => {
         </div>
       </main>
       <footer className="footer container">
-        <a className="footer__logo-link" href={`main.html`}>
+        <Link className="footer__logo-link" to={`/`}>
           <img className="footer__logo" src={`img/logo.svg`} alt="6 cities logo" width="64" height="33"/>
-        </a>
+        </Link>
       </footer>
     </div>
   );
