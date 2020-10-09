38785692a52b37ba0efc3c02a6df5316d43257d2
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-10-09 10:51:02 +0300

    Правит мелкие ошибки в offer.jsx

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -7,8 +7,8 @@ const Offer = () => {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="/main.html">
-                <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              <a className="header__logo-link" href={`main.html`}>
+                <img className="header__logo" src={`img/logo.svg`} alt="6 cities logo" width="81" height="41"/>
               </a>
             </div>
             <nav className="header__nav">
@@ -31,22 +31,22 @@ const Offer = () => {
           <div className="property__gallery-container container">
             <div className="property__gallery">
               <div className="property__image-wrapper">
-                <img className="property__image" src="/img/room.jpg" alt="Photo studio"/>
+                <img className="property__image" src={`img/room.jpg`} alt="Photo studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="/img/apartment-01.jpg" alt="Photo studio"/>
+                <img className="property__image" src={`img/apartment-01.jpg`} alt="Photo studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="/img/apartment-02.jpg" alt="Photo studio"/>
+                <img className="property__image" src={`img/apartment-02.jpg`} alt="Photo studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="/img/apartment-03.jpg" alt="Photo studio"/>
+                <img className="property__image" src={`img/apartment-03.jpg`} alt="Photo studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="/img/studio-01.jpg" alt="Photo studio"/>
+                <img className="property__image" src={`img/studio-01.jpg`} alt="Photo studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="/img/apartment-01.jpg" alt="Photo studio"/>
+                <img className="property__image" src={`img/apartment-01.jpg`} alt="Photo studio"/>
               </div>
             </div>
           </div>
@@ -61,14 +61,14 @@ const Offer = () => {
                 </h1>
                 <button className="property__bookmark-button button" type="button">
                   <svg className="property__bookmark-icon" width="31" height="33">
-                    <use xlinkHref="#icon-bookmark"></use>
+                    <use xlinkHref="#icon-bookmark"/>
                   </svg>
                   <span className="visually-hidden">To bookmarks</span>
                 </button>
               </div>
               <div className="property__rating rating">
                 <div className="property__stars rating__stars">
-                  <span style={{width: `80%`}}></span>
+                  <span style={{width: `80%`}}/>
                   <span className="visually-hidden">Rating</span>
                 </div>
                 <span className="property__rating-value rating__value">4.8</span>
@@ -127,7 +127,7 @@ const Offer = () => {
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
                   <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src="/img/avatar-angelina.jpg" width="74" height="74"
+                    <img className="property__avatar user__avatar" src={`img/avatar-angelina.jpg`} width="74" height="74"
                       alt="Host avatar"/>
                   </div>
                   <span className="property__user-name">
@@ -151,7 +151,7 @@ const Offer = () => {
                   <li className="reviews__item">
                     <div className="reviews__user user">
                       <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="/img/avatar-max.jpg" width="54" height="54"
+                        <img className="reviews__avatar user__avatar" src={`img/avatar-max.jpg`} width="54" height="54"
                           alt="Reviews avatar"/>
                       </div>
                       <span className="reviews__user-name">
@@ -161,7 +161,7 @@ const Offer = () => {
                     <div className="reviews__info">
                       <div className="reviews__rating rating">
                         <div className="reviews__stars rating__stars">
-                          <span style={{width: `80%`}}></span>
+                          <span style={{width: `80%`}}/>
                           <span className="visually-hidden">Rating</span>
                         </div>
                       </div>
@@ -180,7 +180,7 @@ const Offer = () => {
                       type="radio"/>
                     <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
                       <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
+                        <use xlinkHref="#icon-star"/>
                       </svg>
                     </label>
 
@@ -188,7 +188,7 @@ const Offer = () => {
                       type="radio"/>
                     <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
                       <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
+                        <use xlinkHref="#icon-star"/>
                       </svg>
                     </label>
 
@@ -196,7 +196,7 @@ const Offer = () => {
                       type="radio"/>
                     <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
                       <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
+                        <use xlinkHref="#icon-star"/>
                       </svg>
                     </label>
 
@@ -204,7 +204,7 @@ const Offer = () => {
                       type="radio"/>
                     <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
                       <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
+                        <use xlinkHref="#icon-star"/>
                       </svg>
                     </label>
 
@@ -213,12 +213,12 @@ const Offer = () => {
                     <label htmlFor="1-star" className="reviews__rating-label form__rating-label"
                       title="terribly">
                       <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
+                        <use xlinkHref="#icon-star"/>
                       </svg>
                     </label>
                   </div>
                   <textarea className="reviews__textarea form__textarea" id="review" name="review"
-                    placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+                    placeholder="Tell how was your stay, what you like and what can be improved"/>
                   <div className="reviews__button-wrapper">
                     <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and
@@ -230,7 +230,7 @@ const Offer = () => {
               </section>
             </div>
           </div>
-          <section className="property__map map"></section>
+          <section className="property__map map"/>
         </section>
         <div className="container">
           <section className="near-places places">
@@ -239,7 +239,7 @@ const Offer = () => {
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
                   <a href="#">
-                    <img className="place-card__image" src="/img/room.jpg" width="260" height="200" alt="Place image"/>
+                    <img className="place-card__image" src={`img/room.jpg`} width="260" height="200" alt="Place image"/>
                   </a>
                 </div>
                 <div className="place-card__info">
@@ -251,14 +251,14 @@ const Offer = () => {
                     <button className="place-card__bookmark-button place-card__bookmark-button--active button"
                       type="button">
                       <svg className="place-card__bookmark-icon" width="18" height="19">
-                        <use xlinkHref="#icon-bookmark"></use>
+                        <use xlinkHref="#icon-bookmark"/>
                       </svg>
                       <span className="visually-hidden">In bookmarks</span>
                     </button>
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={{width: `80%`}}></span>
+                      <span style={{width: `80%`}}/>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -272,7 +272,7 @@ const Offer = () => {
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
                   <a href="#">
-                    <img className="place-card__image" src="/img/apartment-02.jpg" width="260" height="200"
+                    <img className="place-card__image" src={`img/apartment-02.jpg`} width="260" height="200"
                       alt="Place image"/>
                   </a>
                 </div>
@@ -284,14 +284,14 @@ const Offer = () => {
                     </div>
                     <button className="place-card__bookmark-button button" type="button">
                       <svg className="place-card__bookmark-icon" width="18" height="19">
-                        <use xlinkHref="#icon-bookmark"></use>
+                        <use xlinkHref="#icon-bookmark"/>
                       </svg>
                       <span className="visually-hidden">To bookmarks</span>
                     </button>
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={{width: `80%`}}></span>
+                      <span style={{width: `80%`}}/>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -305,7 +305,7 @@ const Offer = () => {
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
                   <a href="#">
-                    <img className="place-card__image" src="/img/apartment-03.jpg" width="260" height="200"
+                    <img className="place-card__image" src={`img/apartment-03.jpg`} width="260" height="200"
                       alt="Place image"/>
                   </a>
                 </div>
@@ -317,14 +317,14 @@ const Offer = () => {
                     </div>
                     <button className="place-card__bookmark-button button" type="button">
                       <svg className="place-card__bookmark-icon" width="18" height="19">
-                        <use xlinkHref="#icon-bookmark"></use>
+                        <use xlinkHref="#icon-bookmark"/>
                       </svg>
                       <span className="visually-hidden">To bookmarks</span>
                     </button>
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={{width: `100%`}}></span>
+                      <span style={{width: `100%`}}/>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
