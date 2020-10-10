c778f3449be6dd10e145755cc3e10dc8542e21d1
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-10-10 15:03:12 +0300

    Убирает дублирование кода в форме

--- src/components/comment-form/comment-form.jsx
+++ src/components/comment-form/comment-form.jsx
@@ -28,46 +28,18 @@ class CommentForm extends PureComponent {
       >
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
-          <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars"
-            onChange={this.handleFieldChange} type="radio"/>
-          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"/>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars"
-            onChange={this.handleFieldChange} type="radio"/>
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"/>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars"
-            onChange={this.handleFieldChange} type="radio"/>
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"/>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars"
-            onChange={this.handleFieldChange} type="radio"/>
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"/>
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star"
-            onChange={this.handleFieldChange} type="radio"/>
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label"
-            title="terribly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"/>
-            </svg>
-          </label>
+          {[`5`, `4`, `3`, `2`, `1`].map((it) => (
+            <React.Fragment key={it}>
+              <input className="form__rating-input visually-hidden" name="rating" value={it} id={`${it}-stars`}
+                onChange={this.handleFieldChange} type="radio"
+              />
+              <label htmlFor={`${it}-stars`} className="reviews__rating-label form__rating-label" title="perfect">
+                <svg className="form__star-image" width="37" height="33">
+                  <use xlinkHref="#icon-star"/>
+                </svg>
+              </label>
+            </React.Fragment>
+          ))}
         </div>
         <textarea className="reviews__textarea form__textarea" id="review" name="review"
           placeholder="Tell how was your stay, what you like and what can be improved"
