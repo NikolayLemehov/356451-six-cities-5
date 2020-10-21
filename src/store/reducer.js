88c1038f2ccb88c534672a841edd1c5ff2fec29a
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-10-21 23:22:08 +0300

    Исправляет ошибку на Главной при фильтре с пустым списком предложений

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -41,7 +41,7 @@ const reducer = (state = initialState, action) => {
       });
     case ActionType.GET_FIRST_OFFER_ID:
       return extend(state, {
-        overOfferId: state.currentSortedCityOffers[0].id,
+        overOfferId: state.currentSortedCityOffers[0] ? state.currentSortedCityOffers[0].id : false,
       });
   }
   return state;
