0b20e816cd50f802388f2ebbb3ba024261b0bf6f
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-11-08 19:01:36 +0200

    Исправляет hover и фокус у logo

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -33,14 +33,14 @@ class Login extends PureComponent {
           <div className="container">
             <div className="header__wrapper">
               <div className="header__left">
-                <Link className="header__logo-link" to={AppRoute.MAIN}>
+                <Link className="header__logo-link header__logo-link--active" to={AppRoute.MAIN}>
                   <img className="header__logo" src={`/img/logo.svg`} alt="6 cities logo" width="81" height="41"/>
                 </Link>
               </div>
               <nav className="header__nav">
                 <ul className="header__nav-list">
                   <li className="header__nav-item user">
-                    <a className="header__nav-link header__nav-link--profile" href="#">
+                    <a className="header__nav-link header__nav-link--profile" style={{textShadow: `none`, cursor: `default`}}>
                       <div className="header__avatar-wrapper user__avatar-wrapper">
                       </div>
                       <span className="header__login">Sign in</span>
