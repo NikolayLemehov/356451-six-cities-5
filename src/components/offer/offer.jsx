1637b91bac65da76ea5bc623372e7110790298fc
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-11-10 06:06:17 +0200

    Удаляет лишний код в app.jsx и добавляет класс активности для лого в offer.jsx

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -61,7 +61,7 @@ class Offer extends PureComponent {
           <div className="container">
             <div className="header__wrapper">
               <div className="header__left">
-                <Link className="header__logo-link" to={AppRoute.MAIN}>
+                <Link className="header__logo-link header__logo-link--active" to={AppRoute.MAIN}>
                   <img className="header__logo" src={`img/logo.svg`} alt="6 cities logo" width="81" height="41"/>
                 </Link>
               </div>
