3839e24868bff098e8d85c906f45215129f893a3
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-11-14 14:17:37 +0200

    Добавляет user-link.test.js

--- src/components/user-link/user-link.jsx
+++ src/components/user-link/user-link.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {getIsAuthorizedStatus} from "../../store/selectors";
@@ -6,32 +6,39 @@ import {AppRoute} from "../../const";
 import {Link} from "react-router-dom";
 import UserLinkContent from "../user-link-content/user-link-content";
 
-const getLink = (currentRoute, isAuthorizedStatus) => {
-  switch (true) {
-    case currentRoute !== AppRoute.FAVORITES && isAuthorizedStatus:
-      return AppRoute.FAVORITES;
-    case currentRoute !== AppRoute.LOGIN && !isAuthorizedStatus:
-      return AppRoute.LOGIN;
-    default:
-      return false;
+class UserLink extends PureComponent {
+  constructor(props) {
+    super(props);
   }
-};
-const UserLink = (props) => {
-  const {appRoute, isAuthorizedStatus} = props;
-  const link = getLink(appRoute, isAuthorizedStatus);
 
-  return appRoute !== AppRoute.FAVORITES && isAuthorizedStatus || appRoute !== AppRoute.LOGIN && !isAuthorizedStatus ? (
-    <Link className="header__nav-link header__nav-link--profile"
-      to={link}
-    >
-      <UserLinkContent/>
-    </Link>
-  ) : (
-    <a className="header__nav-link header__nav-link--profile header__nav-link--inactive">
-      <UserLinkContent/>
-    </a>
-  );
-};
+  _getLink(currentRoute, isAuthorizedStatus) {
+    switch (true) {
+      case currentRoute !== AppRoute.FAVORITES && isAuthorizedStatus:
+        return AppRoute.FAVORITES;
+      case currentRoute !== AppRoute.LOGIN && !isAuthorizedStatus:
+        return AppRoute.LOGIN;
+      default:
+        return false;
+    }
+  }
+
+  render() {
+    const {appRoute, isAuthorizedStatus} = this.props;
+    const link = this._getLink(appRoute, isAuthorizedStatus);
+
+    return appRoute !== AppRoute.FAVORITES && isAuthorizedStatus || appRoute !== AppRoute.LOGIN && !isAuthorizedStatus ? (
+      <Link className="header__nav-link header__nav-link--profile"
+        to={link}
+      >
+        <UserLinkContent/>
+      </Link>
+    ) : (
+      <a className="header__nav-link header__nav-link--profile header__nav-link--inactive">
+        <UserLinkContent/>
+      </a>
+    );
+  }
+}
 
 UserLink.propTypes = {
   appRoute: PropTypes.string.isRequired,
