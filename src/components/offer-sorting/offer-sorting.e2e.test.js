6dfa36c5fa9fc1f555a6c1a08581d7eea7380053
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-11-17 01:18:17 +0200

    Делает offer-sorting.e2e.test.js

---
+++ src/components/offer-sorting/offer-sorting.e2e.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import OfferSorting from "./offer-sorting";
+
+configure({adapter: new Adapter()});
+
+test(`click on OfferSorting correctly`, () => {
+  const onCloseMenu = jest.fn();
+  const onToggleMenu = jest.fn();
+  const wrapper = shallow(
+      <OfferSorting
+        isOpened={false}
+        onCloseMenu={onCloseMenu}
+        onToggleMenu={onToggleMenu}
+      />
+  );
+
+  wrapper.find(`span.places__sorting-type`).simulate(`click`);
+  expect(onToggleMenu).toHaveBeenCalledTimes(1);
+});
+
